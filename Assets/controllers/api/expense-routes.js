7487550ab3772a437653f39556f2fba99089da4d
Shawn Canavan <105885313+shawnbandy@users.noreply.github.com>
2022-10-12 19:13:40 -0400

    commit

---
+++ Assets/controllers/api/expense-routes.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+const { User, Expense, Household, Income } = require('../../models');
+
+router.post('/create', async (req, res) => {
+  try {
+    const newExpense = Expense.create({
+      name: req.body.name,
+      amount: req.body.amount,
+      household_id: req.body.household_id,
+    });
+
+    res.status(200).json(newExpense);
+  } catch (err) {
+    res.status(404).json({ message: 'Unable to create a new expense. ' + err });
+  }
+});
+
+router.put('/edit/:id', async (req, res) => {
+  try {
+    const updateExpense = await Expense.update(
+      {
+        name: req.body.name,
+        amount: req.body.amount,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).json(updateExpense);
+  } catch (err) {
+    res.status(404).json({ message: 'Unable to create a new expense. ' + err });
+  }
+});
+
+router.delete('/delete/:id', async (req, res) => {
+  try {
+    const deleteExpense = await Expense.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json({ message: 'Deleted' });
+  } catch {
+    res.status(404).json({ message: 'Unable to create a new Expense. ' + err });
+  }
+});
