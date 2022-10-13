1fc2081dc54f6aa1d793437149fa47c752fb4009
Shawn Canavan <105885313+shawnbandy@users.noreply.github.com>
2022-10-13 10:32:31 -0400

    added code to login/logout, created signup.js, fixed some minor bugs, got some routes working as intended

--- Assets/public/js/logout.js
+++ Assets/public/js/logout.js
@@ -1,1 +1,15 @@
 // Add an event listener to the logout button
+const logout = async () => {
+  const res = await fetch('/api/user/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
+  if (res.ok) {
+    document.location.replace('/');
+  } else {
+    alert('Failed to log out');
+  }
+};
+
+//!add it to the logout button
+document.querySelector('#logout').addEventListener('click', logout);
