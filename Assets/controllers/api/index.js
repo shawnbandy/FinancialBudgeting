50f53c66763e23ac838e6c8303962f11b8a37650
Shawn Canavan <105885313+shawnbandy@users.noreply.github.com>
2022-10-12 20:02:25 -0400

    fixed small bug

--- Assets/controllers/api/index.js
+++ Assets/controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const expenseRoutes = require('./expense-routes.js');
-const incomeRoutes = require('./income-routes');
+const incomeRoutes = require('./income-routes.js');
 
 //*this is where we link the routers with other parts of api
 //*eg, router.use('/location', locationRoutes)
