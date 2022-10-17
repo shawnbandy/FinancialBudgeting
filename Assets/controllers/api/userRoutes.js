d1d0d7e5a8435e73a800345c626fe0c0fe1543b7
Indigo <indigo.crandell@gmail.com>
2022-10-17 14:56:24 -0400

    m

--- Assets/controllers/api/userRoutes.js
+++ Assets/controllers/api/userRoutes.js
@@ -7,12 +7,12 @@ router.post('/', async (req, res) => {
   console.log('reached');
   try {
     const newUser = await User.create({
-      username: req.body.username,
+      username: req.body.email,
       password: req.body.password,
     });
     req.session.save(() => {
       req.session.userId = newUser.id; //!
-      req.session.username = newUser.username; //!
+      req.session.email = newUser.email; //!
       req.session.loggedIn = true;
       res.json(newUser);
     });
@@ -27,26 +27,26 @@ router.post('/login', async (req, res) => {
   try {
     const user = await User.findOne({
       where: {
-        username: req.body.username,
+        username: req.body.email,
       },
     });
     if (!user) {
-      res.status(400).json({ message: 'Invalid username' });
+      res.status(400).json({ message: 'Invalid username or password' });
       return;
     }
     const validPassword = user.checkPassword(req.body.password);
     if (!validPassword) {
-      res.status(400).json({ message: 'Invalid password' });
+      res.status(400).json({ message: 'Invalid username or password' });
       return;
     }
     req.session.save(() => {
       req.session.userId = user.id;
-      req.session.username = user.username;
+      req.session.email = user.email;
       req.session.loggedIn = true;
       res.json({ user, message: 'You are now logged in' });
     });
   } catch (err) {
-    res.status(400).json({ message: 'Oops! Something went wrong!' });
+    res.status(400).json({ message: 'Invalid username or password' });
   }
 });
 
@@ -62,3 +62,4 @@ router.post('/logout', (req, res) => {
 });
 
 module.exports = router;
+
