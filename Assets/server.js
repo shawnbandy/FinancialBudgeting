e6b2a019b1700452fe230c0c189c45af37b1a682
parson40 <101997352+parsonjd@users.noreply.github.com>
2022-10-17 20:28:59 -0400

    added changes

--- Assets/server.js
+++ Assets/server.js
@@ -25,20 +25,19 @@ const sess = {
   }),
 };
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sess));
+app.use(routes);
+
 
 const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(routes);
-
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () =>
     console.log(
       `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`
