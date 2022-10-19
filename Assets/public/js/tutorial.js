c3cdd70d28613bd7ee93f8d60dc0f803ca12f8f9
Indigo <indigo.crandell@gmail.com>
2022-10-19 16:13:02 -0400

    Tentative javascript/handlebars for a VERY basic tutorial. Probably not necessary. But could be fun.

--- Assets/public/js/tutorial.js
+++ Assets/public/js/tutorial.js
@@ -1,14 +1,55 @@
 // Functionality for going through the tutorial
 const nextBtn = document.querySelector("#nextTutorial");
-const tutorial1 = document.querySelector("#tutorial-1");
-const tutorial2 = document.querySelector("#tutorial-2");
+const tutorial1 = document.querySelector("#tutorial-1"); // first tutorial item not part of the loop
+const tutorialArr = [];
 
-const nextTutorial = (event) => {
+// i is first defined as 0
+var i = 0;
+
+const goToDashboard = async () => {
+// function to go to dashboard
+const res = await fetch('/dashboard', {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+});
+if(res.ok) {
+    document.location.replace('/dashboard');
+}
+}
+
+
+const nextTutorial = async (event) => {
     event.preventDefault();
 
-    console.log('next slide');
+    if (i !== 2) {
+    // set first tutorial item to hidden
     tutorial1.setAttribute('class', 'hidden');
-    tutorial2.removeAttribute('class', 'hidden');
+
+    // loop through each of the following tutorial items
+    const elements = document.querySelectorAll("[data-id]");
+    
+    elements.forEach(el => {
+        // push each tutorial item to an array defined in the global scope
+        tutorialArr.push(el);
+    });
+
+    // Show current tutorial item
+    tutorialArr[i].removeAttribute('class', 'hidden');
+    // i is now defined as i + 1
+    i++;
+
+    // don't really know how this is working?
+    tutorialArr[i].setAttribute('class', 'hidden');
+
+    } else {
+    // Once all three tutorial elements have popped up, change next button to dashboard button
+        console.log('end of tutorial!');
+        nextBtn.textContent = 'Go to Dashboard!';
+        // call function to go to dashboard
+        //nextBtn.setAttribute('href', '/dashboard');
+
+        goToDashboard();
+    }
 }
 
 nextBtn.addEventListener('click', nextTutorial);
